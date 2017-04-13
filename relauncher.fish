aadf56133d57d543d7ebda148bcb13a915deafe9
nevabyte <Carl@Carls-MacBook-Pro.local>
2017-04-13 19:45:20 +0100

    Updated getinfo function name to relauncher

--- relauncher.fish
+++ relauncher.fish
@@ -1,6 +1,6 @@
-# Rebuild Get Info for Finder
+# Rebuild Launch Services for Mac
 
-function getinfo
+function relauncher
 
   /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
   killall Dock
